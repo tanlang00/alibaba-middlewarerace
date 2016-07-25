7238efaa89f89346b5ed64e97ab5b72645965a73
tanlang00 <kuangshenyuan@126.com>
2016-07-25 15:32:10 +0800

    Update RaceTopology.java

--- src/main/java/com/alibaba/middleware/race/jstorm/RaceTopology.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/RaceTopology.java
@@ -19,26 +19,22 @@ public class RaceTopology {
 
         Config conf = new Config();
         conf.setDebug(false);
-        int spout_Parallelism_hint = 1;
-        int split_Parallelism_hint = 1;
-        int count_Parallelism_hint = 1;
+        int spout_Parallelism_hint = 4;
+        int split_Parallelism_hint = 2;
+        int count_Parallelism_hint = 2;
         int count_workers = 4;
 
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout("spout", new RockMqSpout(), spout_Parallelism_hint);
-//        builder.setSpout("spout3", new RatioSpout(), spout_Parallelism_hint);
-//        builder.setSpout("spout1", new TaoBaoSpout(), spout_Parallelism_hint);
-//        builder.setSpout("spout2", new TianMaoSpout(), spout_Parallelism_hint);
-        PreparedBolt b = new PreparedBolt();
-//        builder.setBolt("bolt1_1", b,count_Parallelism_hint).shuffleGrouping("spout",RaceConfig.TAOBAO_ID);
-//        builder.setBolt("bolt1_2", b,count_Parallelism_hint).shuffleGrouping("spout",RaceConfig.TIANMAO_ID);
-        builder.setBolt("bolt1_3", b,count_Parallelism_hint).shuffleGrouping("spout",RaceConfig.RATIO_ID);
+          //分属于不同worker，导致数据处理失败，数据已经分流了，bolt中的逻辑无法完成
+          //如要修改，则应该将preparedBolt集中到一个worker中，保证其中共享变量有效，则应该采用一个bolt，多个线程，就不会造成错误。
+        builder.setBolt("bolt1_1", new PreparedBolt(),count_Parallelism_hint).shuffleGrouping("spout",RaceConfig.TAOBAO_ID);
+        builder.setBolt("bolt1_2", new PreparedBolt(),count_Parallelism_hint).shuffleGrouping("spout",RaceConfig.TIANMAO_ID);
+        builder.setBolt("bolt1_3", new PreparedBolt(),count_Parallelism_hint).shuffleGrouping("spout",RaceConfig.RATIO_ID);
         builder.setBolt("bolt2", new CalTTBolt(),split_Parallelism_hint).shuffleGrouping("bolt1_1");
         builder.setBolt("bolt3", new CalTTBolt(),split_Parallelism_hint).shuffleGrouping("bolt1_2");
         builder.setBolt("bolt4", new CalTTBolt(),split_Parallelism_hint).shuffleGrouping("bolt1_3");
-        builder.setBolt("bolt5", new RatioBolt(),split_Parallelism_hint).shuffleGrouping("spout",RaceConfig.RATIO_ID);
-//        builder.setSpout("spout1", new RatioSpout(),1);
-//        builder.setBolt("bolt", new RatioBolt(),6).shuffleGrouping("spout1",RaceConfig.RATIO_ID);
+        builder.setBolt("bolt5", new RatioBolt(),3).shuffleGrouping("spout",RaceConfig.RATIO_ID);
         String topologyName = RaceConfig.JstormTopologyName;
         
         Config.setNumAckers(conf, 0);
@@ -47,9 +43,9 @@ public class RaceTopology {
         
         
         try {
-//            StormSubmitter.submitTopology(topologyName, conf, builder.createTopology());
-        	LocalCluster cluster = new LocalCluster();
-        	cluster.submitTopology(topologyName, conf, builder.createTopology());
+            StormSubmitter.submitTopology(topologyName, conf, builder.createTopology());
+        //	LocalCluster cluster = new LocalCluster();
+        //	cluster.submitTopology(topologyName, conf, builder.createTopology());
 //        	Thread.sleep(30000);
 //        	cluster.shutdown();
         } catch (Exception e) {
